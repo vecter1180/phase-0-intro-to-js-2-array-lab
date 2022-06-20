eb87e59a4ac687a0082fc6bee82dd8e06a76a03a
Brian Allen <vecter1180@gmail.com>
2022-06-20 01:12:37 -0400

    commit tree 05291512e398fcb73eb3d78650bdcdace4450c8f

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(Bob){
+    cats.pop(2)
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo")
+}
+
+
+
+
+let moreCats = [...cats, "Broom"]
+let moreCats3 = [...cats]
+let moreCats4 = [...cats]
+
+function appendCat(){
+    cats.slice("Broom")
+    return moreCats
+}
+
+function prependCat(){
+    moreCats.pop("Broom")
+    moreCats.unshift("Arnold")
+    return moreCats
+}
+
+function removeLastCat(){
+    moreCats4.pop("Garfield")
+    
+
+  return moreCats4
+  }
+
+function removeFirstCat(){
+    moreCats3.shift("Milo")
+     
+      return moreCats3
+  }
